d16eb3244dd33b6cb5d4ca58638e0a47b75ce397
jackmash <jackmacha@gmail.com>
2026-07-27 16:37:33 +0300

    Add Junior Secondary master subject config with 9 official CBC subjects and codes

---
+++ backend/config/subjects.js
@@ -0,0 +1,15 @@
+// Master subject configuration for Junior Secondary (Grade 7 - Grade 9)
+// CBC official 9-subject list with fixed order and subject codes
+const JUNIOR_SECONDARY_SUBJECTS = [
+  { code: '901', name: 'English', shortName: 'ENG' },
+  { code: '902', name: 'Kiswahili', shortName: 'KIS' },
+  { code: '903', name: 'Maths', shortName: 'MAT' },
+  { code: '905', name: 'Science', shortName: 'SCIE' },
+  { code: '906', name: 'Agriculture', shortName: 'AGR' },
+  { code: '907', name: 'Social Studies', shortName: 'SST' },
+  { code: '908', name: 'CRE', shortName: 'CRE' },
+  { code: '911', name: 'Creative Arts & Sports', shortName: 'C.A.S' },
+  { code: '912', name: 'Pre-Technical', shortName: 'PRE' }
+];
+
+module.exports = { JUNIOR_SECONDARY_SUBJECTS };
